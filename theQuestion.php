f6d69e3dc914a7b38572d37ccd471c447143138b
montysavage <mg7een@hotmail.com>
2013-02-28 21:18:17 -0500

    Update theQuestion.php

--- theQuestion.php
+++ theQuestion.php
@@ -1,1 +1,16 @@
-/** *  Truth! That is the Question. * * @param $b * @return that * */function truDat($b) {	$theQuestion = (2*$b || !2*$b);		$that = $theQuestion;	return $that;	}
+/**
+ *  Truth! That is the Question.
+ *
+ * @author montys
+ * @param $b
+ * @return that
+ *
+ */
+function truDat($b) {
+
+	$theQuestion = (2*$b || !2*$b);	
+	$that = $theQuestion;
+
+	return $that;
+	
+}
