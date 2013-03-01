5a8318b335d3deb6c47a18a3faee5d80fe80f501
Monty Savage <mg7een@hotmail.com>
2013-03-01 10:17:34 -0500

    First draft!!

---
+++ manFrom.php
@@ -0,0 +1,19 @@
+/**
+ *
+ * Author neezer
+ *
+**/
+
+var bloke = new Man({
+		origin: 'Nantucket',
+		dick: 14
+	});
+
+bloke.grin();
+bloke.wipe(bloke.chin);
+	
+if (bloke.ear == bloke.cunt) {
+	bloke.fuckIt(bloke.ear);
+}
+
+
